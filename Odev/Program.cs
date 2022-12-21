30132fa0d4ebaceb92c703e0eb22618fd3db8402
dilbe <dilbe@MONSTERS>
2022-12-21 23:13:50 +0300

    Proje dosyası ekle.

---
+++ Odev/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Bir dikdörtgen alanını ve  çevresini hesaplamak için gerekli olan bilgileri kullanıcıdan düzgün bir şekilde alınız
+        // ve sonucunu ekrana düzgün şekilde yazınız
+
+
+        int kenar1, kenar2, alan =0 ,cevre=0 ;
+        Console.WriteLine("ilk sayıyı giriniz :");
+        kenar1= Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ikinci sayıyı giriniz :");
+        kenar2= Convert.ToInt32(Console.ReadLine());
+
+        alan= (kenar1*kenar2);
+        Console.WriteLine("Dikdörtgen alanı :" + alan);
+
+        cevre = (kenar1*2) + (kenar2*2);
+        Console.WriteLine("Dikdörtgenin çevre uzunlugu :" + cevre);
+
+
+
+        Console.ReadKey();
+
+      
+
+    }
+}
---
+++ WorkOut/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int number = 0;
+        Console.WriteLine("lütfen bir sayı giriniz :");
+
+        number = int.Parse(Console.ReadLine()); // önce ekrandan okunuyor. Anında pars metodu ile int'e çevrilir
+
+        Console.WriteLine("Girdiğiniz olduğunuz sayı: {0}", number);
+
+        Console.WriteLine("lütfen programdan çıkmak için bir tuşa basınız...");
+
+        Console.ReadKey();
+    }
+}
